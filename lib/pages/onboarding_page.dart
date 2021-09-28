995d8632a566c7bd645b381be244378f4e7145f5
Arief Syahroni <riefsyah31@gmail.com>
2021-09-28 08:35:42 +0700

    done set up and done modified onboardingPage and add some assets or support

---
+++ lib/pages/onboarding_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+import '../theme.dart';
+
+class Onboardingpaage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: utilityDarkBackgroundColor,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 38,
+              ),
+              Text(
+                'Your Favorite Food, Delivered Fast',
+                style: headingThree.copyWith(
+                  color: inkWhiteColor,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                'Find the best restaurants in your city and get\nit delivered to your place!',
+                style: paragraphMedium.copyWith(
+                  color: inkWhiteColor,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Image.asset(
+                'assets/illustration.png',
+                width: double.infinity,
+                height: 386,
+              ),
+              Spacer(),
+              Container(
+                height: 56,
+                width: MediaQuery.of(context).size.width - (2 * 72),
+                child: RaisedButton(
+                  onPressed: () {},
+                  color: utilityWarningColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: Align(
+                    child: Text(
+                      'Order Now!',
+                      style: componentsLarge.copyWith(
+                        color: inkDarkColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color inkDarkColor = Color(0XFF191D21);
+Color inkDarkGreyColor = Color(0XFF656F77);
+Color inkSemiDarkGreyColor = Color(0XFFACB8C2);
+Color inkGreyColor = Color(0XFFCDD9E3);
+Color inkLightGreyColor = Color(0XFFE8EEF3);
+Color inkWhiteColor = Color(0XFFFFFFFF);
+
+Color utilityDarkBackgroundColor = Color(0XFF393939);
+Color utilitySuccessColor = Color(0XFF23E9B4);
+Color utilityErrorColor = Color(0XFFFF5A5A);
+Color utilityWarningColor = Color(0XFFFFAC4B);
+Color utilityInfoColor = Color(0XFF91D7E0);
+Color utilityActiveColor = Color(0XFF1814E4);
+
+Color accentPinkColor = Color(0XFFFDC9D2);
+Color accentPurpleColor = Color(0XFFDED2F9);
+Color accentBlueColor = Color(0XFFC9EAFD);
+Color accentGreenColor = Color(0XFFD0F1EB);
+Color accentYellowColor = Color(0XFFFDEBC9);
+Color accentOrangeColor = Color(0XFFF5D4C1);
+
+TextStyle headingZero = GoogleFonts.raleway(
+  fontWeight: FontWeight.bold,
+  fontSize: 72,
+  letterSpacing: -1,
+);
+
+TextStyle headingOne = GoogleFonts.raleway(
+  fontWeight: FontWeight.bold,
+  fontSize: 56,
+  letterSpacing: -1,
+);
+
+TextStyle headingTwo = GoogleFonts.raleway(
+  fontWeight: FontWeight.bold,
+  fontSize: 40,
+  letterSpacing: -1,
+);
+
+TextStyle headingThree = GoogleFonts.raleway(
+  fontWeight: FontWeight.bold,
+  fontSize: 32,
+  letterSpacing: -1,
+);
+
+TextStyle headingFour = GoogleFonts.raleway(
+  fontWeight: FontWeight.bold,
+  fontSize: 24,
+  letterSpacing: -0.5,
+);
+
+TextStyle headingFive = GoogleFonts.raleway(
+  fontWeight: FontWeight.bold,
+  fontSize: 20,
+  letterSpacing: -0.5,
+);
+
+TextStyle paragraphLarge = GoogleFonts.raleway(
+  fontWeight: FontWeight.w400,
+  fontSize: 16,
+  letterSpacing: -0.5,
+);
+
+TextStyle paragraphLargeBold = GoogleFonts.raleway(
+  fontWeight: FontWeight.bold,
+  fontSize: 16,
+  letterSpacing: -0.5,
+);
+
+TextStyle paragraphMedium = GoogleFonts.raleway(
+  fontWeight: FontWeight.w300,
+  fontSize: 14,
+  letterSpacing: -0.5,
+);
+
+TextStyle paragraphMediumBold = GoogleFonts.raleway(
+  fontWeight: FontWeight.bold,
+  fontSize: 14,
+  letterSpacing: -0.5,
+);
+
+TextStyle paragraphSmall = GoogleFonts.raleway(
+  fontWeight: FontWeight.w400,
+  fontSize: 12,
+  letterSpacing: -0.5,
+);
+
+TextStyle paragraphSmallBold = GoogleFonts.raleway(
+  fontWeight: FontWeight.bold,
+  fontSize: 12,
+  letterSpacing: -0.5,
+);
+
+TextStyle componentsLarge = GoogleFonts.raleway(
+  fontWeight: FontWeight.bold,
+  fontSize: 16,
+  letterSpacing: -0.5,
+);
+
+TextStyle componentsMedium = GoogleFonts.raleway(
+  fontWeight: FontWeight.w400,
+  fontSize: 14,
+);
+
+TextStyle componentsMediumBold = GoogleFonts.raleway(
+  fontWeight: FontWeight.bold,
+  fontSize: 14,
+);
+
+TextStyle componentsSmal = GoogleFonts.raleway(
+  fontWeight: FontWeight.bold,
+  fontSize: 12,
+);
